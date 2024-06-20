79d193869c3b00c370c95c463df65ffda1719580
Caio Costa <caiocosta22@gmail.com>
2024-06-20 14:46:49 -0300

    Reorganizando arquivos e adicionada logica das box

---
+++ src/components/Conteudo/Experiencia.vue
@@ -0,0 +1,132 @@
+<script setup>
+import { ref } from "vue";
+
+const jobs = ref([
+  {
+    empresa: "Agência Alastrar",
+    titulo: "Desenvolvedor Fullstack",
+    descricao: "",
+    icons: [
+      {
+        name: ""
+      }
+    ],
+    tempo: "10/2023 - Atuando"
+  },
+  {
+    empresa: "G2 Sistemas",
+    titulo: "Desenvolvedor Back-End",
+    descricao: "",
+    icons: [
+      {
+        name: ""
+      }
+    ],
+    tempo: "07/2023 - 10/2023"
+  },
+  {
+    empresa: "G2 Sistemas",
+    titulo: "Estagiário de Suporte",
+    subtitulo: "Analista de Suporte",
+    descricao: "",
+    icons: [
+      {
+        name: ""
+      }
+    ],
+    tempo: "11/2021 - 07/2023"
+  },
+  {
+    empresa: "Instituto Federal do Ceará",
+    titulo: "Bolsista",
+    descricao: "",
+    icons: [
+      {
+        name: ""
+      }
+    ],
+    tempo: "04/2019 - 08/2019"
+  }
+]);
+</script>
+
+<template lang="pug">
+div.box(
+  v-for="job in jobs"
+  :key="job"
+)
+  h6 {{ job.empresa }}
+  div.grid
+    div.textos
+      p {{ job.descricao }}
+    div.cargo
+      q-separator(
+        vertical
+        color="warning"
+      )
+      div.flex.column
+        h6.titulo-cargo {{ job.titulo }} <br>
+        h6.titulo-cargo(
+          v-if="job.subtitulo"
+        ) {{ job.subtitulo }}
+          div.icones
+            q-icon(
+              v-for="icon in job.icons"
+              :key="icon"
+              :name="icon.name"
+              size="md"
+            )
+  span {{ job.tempo }} <br>
+  span
+</template>
+
+<style scoped>
+* {
+  color: #141414;
+  line-height: 1.25;
+}
+
+p, h6 {
+  padding: 0px;
+  margin: 0px
+}
+
+span {
+  padding-left: 20px;
+}
+
+.box {
+  background-color: #fff;
+  border-radius: 20px;
+  width: 100%;
+  margin-bottom: 20px
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  padding: 20px
+}
+
+.textos {
+  display: flex;
+  flex-direction: column;
+  gap: 20px
+}
+
+h6 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  padding: 20px 0px 0px 20px;
+}
+
+.cargo {
+  display: flex;
+  gap: 15px
+}
+
+.titulo-cargo {
+  padding: 0px;
+  font-size: 1rem;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,8 @@
 import Titulo from "src/components/Lateral/Titulo.vue";
 import Menu from "src/components/Lateral/Menu.vue";
 import Sociais from "src/components/Lateral/Sociais.vue";
+
+import Experiencia from "src/components/Conteudo/Experiencia.vue";
 </script>
 
 <template lang="pug">
@@ -10,13 +12,15 @@ section.principal
     Titulo
     Menu
     Sociais
+  section.conteudo
+    Experiencia
 </template>
 
 <style scoped>
 .principal {
   display: grid;
   grid-template-columns: 1fr 4fr;
-  gap: 100px;
+  gap: 150px;
   height: 100%;
 }
 
