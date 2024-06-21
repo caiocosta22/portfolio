a2663bf05ae4658d0b2962bf5abdc2f4d328094c
Caio Costa <caiocosta22@gmail.com>
2024-06-21 20:38:37 -0300

    pdf adicionado

--- src/components/Conteudo/Sobre.vue
+++ src/components/Conteudo/Sobre.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from "vue";
+
+const pdf = ref("https://drive.google.com/file/d/103eyPu_UzIym3jMrR73-DmYe7TZtzyMQ/view?usp=sharing");
 </script>
 
 <template lang="pug">
@@ -6,11 +9,17 @@ p Como uma clássica história de desenvolvedor de software, minha paixão pelo 
 p  Já trabalhei em produtos de pequeno e grande escopo como landing pages, hotsites e ecommerce para distribuidoras, lojistas, incorporadoras e construtoras. No desenvolvimento, atualmente, utilizo a tecnologia Vue 3 junto ao Quasar Framework, Figma Design para protótipos e KANBAN como workflow.
 p Tenho 22 anos, curso Análise e Desenvolvimento de Sistemas, estou estudando React com Typescript pela plataforma Origamid e participando do programa Santander Coders 2024, na trilha de Front-End, para conseguir conhecimentos em Angular.
 
-q-btn(
-  color="info"
-  style="border-radius:20px; border:1px solid #141414; box-shadow:none"
+a(
+  download="CurriculoCaioCosta.pdf"
+  :href="pdf"
+  target="_blank"
+  alt="Currículo"
 )
-  span Download CV
+  q-btn(
+    color="info"
+    style="border-radius:20px; border:1px solid   #141414; box-shadow:none"
+  )
+    span Download CV
 </template>
 
 <style scoped>
