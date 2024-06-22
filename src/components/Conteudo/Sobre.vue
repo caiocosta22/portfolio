c48d7584de82493ef691812f91a00cc834259be6
Caio Costa <caiocosta22@gmail.com>
2024-06-21 21:01:37 -0300

    ajuste de texto

--- src/components/Conteudo/Sobre.vue
+++ src/components/Conteudo/Sobre.vue
@@ -5,10 +5,9 @@ const pdf = ref("https://drive.google.com/file/d/103eyPu_UzIym3jMrR73-DmYe7TZtzy
 </script>
 
 <template lang="pug">
-p Como uma clássica história de desenvolvedor de software, minha paixão pelo que faço nasce na infância repleta de tecnologia e aquela aspiração de poder trabalhar com isso um dia. Porém, sempre fui encantado por interfaces limpas, como elas são construídas e como o usuário se sente ao interagir. Meu principal objetivo é que você sinta um pouco isso também ao visualizar meus produtos. <br>
-p  Já trabalhei em produtos de pequeno e grande escopo como landing pages, hotsites e ecommerce para distribuidoras, lojistas, incorporadoras e construtoras. No desenvolvimento, atualmente, utilizo a tecnologia Vue 3 junto ao Quasar Framework, Figma Design para protótipos e KANBAN como workflow.
-p Tenho 22 anos, curso Análise e Desenvolvimento de Sistemas, estou estudando React com Typescript pela plataforma Origamid e participando do programa Santander Coders 2024, na trilha de Front-End, para conseguir conhecimentos em Angular.
-
+p Como em uma clássica história de desenvolvedor de software, minha paixão nasceu ainda na infância, repleta de novas tecnologias, despertando a aspiração de, um dia, poder trabalhar com isso. Sempre fui encantado por interfaces limpas, suas construções e as sensações que provocam no usuário. Assim, meu principal objetivo é transmitir essa experiência nos meus produtos.
+p Já trabalhei em projetos de pequeno e grande porte, como landing pages, hotsites e e-commerce, para uma ampla gama de empresas, como distribuidoras, lojistas, incorporadoras e construtoras. Nesses projetos, utilizei a tecnologia Vue 3, junto com o Quasar Framework, Figma Design para criação de protótipos e o método KANBAN como workflow.
+p Tenho 22 anos, estou cursando Análise e Desenvolvimento de Sistemas, estudo React com Typescript pela plataforma Origamid e participo do programa Santander Coders 2024, na trilha de Front-End, para ampliar meus conhecimentos em Angular.
 a(
   download="CurriculoCaioCosta.pdf"
   :href="pdf"
