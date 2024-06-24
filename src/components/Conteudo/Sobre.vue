8c09d6ab0776dfa450f9cff0d2beeb00f60c686b
Caio Costa <caiocosta22@gmail.com>
2024-06-24 15:53:45 -0300

    mudanças no texto

--- src/components/Conteudo/Sobre.vue
+++ src/components/Conteudo/Sobre.vue
@@ -5,9 +5,9 @@ const pdf = ref("https://drive.google.com/file/d/103eyPu_UzIym3jMrR73-DmYe7TZtzy
 </script>
 
 <template lang="pug">
-p Como em uma clássica história de desenvolvedor de software, minha paixão nasceu ainda na infância, repleta de novas tecnologias, despertando a aspiração de, um dia, poder trabalhar com isso. Sempre fui encantado por interfaces limpas, suas construções e as sensações que provocam no usuário. Assim, meu principal objetivo é transmitir essa experiência nos meus produtos.
-p Já trabalhei em projetos de pequeno e grande porte, como landing pages, hotsites e e-commerce, para uma ampla gama de empresas, como distribuidoras, lojistas, incorporadoras e construtoras. Nesses projetos, utilizei a tecnologia Vue 3, junto com o Quasar Framework, Figma Design para criação de protótipos e o método KANBAN como workflow.
-p Tenho 22 anos, estou cursando Análise e Desenvolvimento de Sistemas, estudo React com Typescript pela plataforma Origamid e participo do programa Santander Coders 2024, na trilha de Front-End, para ampliar meus conhecimentos em Angular.
+p Como em uma clássica história de desenvolvedor de software, minha paixão nasceu ainda na infância, repleta de novas tecnologias, despertando a aspiração de, um dia, poder trabalhar com isso. Sempre fui encantado por <b>interfaces limpas e interativas </b>, suas construções e as sensações que provocam no usuário. Assim, meu principal objetivo é transmitir essa experiência nos meus produtos.
+p Já trabalhei em projetos de pequeno e grande porte, como <b>landing pages, hotsites e e-commerce</b>, para uma ampla gama de empresas, como <b>distribuidoras, lojistas, incorporadoras e construtoras</b>. Nesses projetos, utilizei a tecnologia <b>Vue 3</b>, junto com o <b>Quasar Framework</b>, <b>Figma Design</b> para criação de protótipos e o método <b>KANBAN</b> como workflow.
+p Tenho 22 anos, estou cursando <b>Análise e Desenvolvimento de Sistemas</b>, estudo <b>React com Typescript</b> pela plataforma Origamid e participo do programa <b>Santander Coders 2024</b>, na trilha de Front-End, para ampliar meus conhecimentos em Angular.
 a(
   download="CurriculoCaioCosta.pdf"
   :href="pdf"
