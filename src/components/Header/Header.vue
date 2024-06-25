281de0493bf175251349feee0d172dfdb4ba3b10
Caio Costa <caiocosta22@gmail.com>
2024-06-25 16:13:40 -0300

    adicionado menu responsivo temporario

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -9,6 +9,27 @@ const switchTeme = () => {
   $q.dark.toggle();
 };
 
+const menus = ref([
+  {
+    menu: "sobre",
+    link: ""
+  },
+  {
+    menu: "experiência",
+    link: ""
+  },
+  {
+    menu: "projetos",
+    link: ""
+  },
+  {
+    menu: "formação",
+    link: ""
+  }
+]);
+
+const drawer = ref(false);
+
 const iconColor = computed(() => {
   return toggle.value ? "primary" : "secondary";
 });
@@ -39,6 +60,42 @@ div.header
       name="fa-solid fa-moon"
       :color="iconColor"
     )
+  div.hamburger
+    q-icon(
+      name="fa-solid fa-bars"
+      :color="iconColor"
+      size="xs"
+      @click="drawer = !drawer"
+      round
+      dense
+    )
+div.menu
+  q-drawer(
+    behavior="mobile"
+    v-model="drawer"
+    :width="200"
+    :breakpoint="1080"
+    bordered
+    elevated
+    side="right"
+  )
+    q-scroll-area(
+      class="fit"
+    )
+      q-list
+        q-separator
+        template(
+          v-for="item in menus"
+          :key="item"
+        )
+          q-item(
+            clickable :active="item.menu === 'Outbox'" v-ripple
+          )
+            q-item-section.texto(
+              style="padding-left: 0; padding-top: 0; text-align: center;"
+              @click="goTo(item.link)"
+            ) {{ item.menu }}
+          q-separator
 </template>
 
 <style scoped>
@@ -53,5 +110,31 @@ div.header
   flex-direction: row;
   gap: 5px;
   align-items: center
+}
+
+.hamburger {
+  display: flex;
+  align-items: center;
+  padding: 15px 10px 10px 10px
+}
+
+@media screen and (min-width:1080px) {
+  .hamburger {
+    display: none;
+  }
+
+  .menu {
+    display: none;
+  }
+}
+
+@media screen and (max-width:1080px) {
+  .icons {
+    display: none;
+  }
+
+  img {
+    max-width: 70%;
+  }
 }
 </style>
