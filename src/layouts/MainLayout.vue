d60ab040667414912d2e040c8f942f1a84eb8bdc
Caio Costa <caiocosta22@gmail.com>
2024-06-26 13:28:27 -0300

    adicionado botao ao layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,30 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
 import Header from "src/components/Header/Header.vue";
+
+const isButtonVisible = ref(false);
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+
+const handleScroll = () => {
+  isButtonVisible.value = window.scrollY !== 0;
+};
+
+const scrollTop = () => {
+  window.scrollTo(
+    {
+      top: 0,
+      behavior: "smooth"
+    })
+  ;
+};
 </script>
 
 <template lang="pug">
@@ -12,6 +37,17 @@ q-layout.container(
     Header
   q-page-container.pagina
     router-view
+  q-btn.floating-button2(
+    color="info"
+    position="bottom-right"
+    @click="scrollTop"
+    align="right"
+    v-if="isButtonVisible"
+    size="sm"
+  )
+    q-icon(
+      name="arrow_upward"
+    )
 </template>
 
 <style scoped>
@@ -31,9 +67,27 @@ q-layout.container(
   height: 80vh;
 }
 
+.floating-button2 {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  z-index: 9999;
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  align-items: center;
+}
+
 @media screen and (max-width:1248px) {
   .container {
     padding: 0px 20px;
+  }
+}
+
+@media screen and (max-width:1080px) {
+  .floating-button2 {
+    display: none;
   }
 }
 
